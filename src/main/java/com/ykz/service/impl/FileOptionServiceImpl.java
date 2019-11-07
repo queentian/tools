ca72c5666261e7a8c6fd59b1467520d852985791
queentian <740257536@qq.com>
2019-11-08 01:13:50 +0800

    新增生成配置文件的controller和实现文件上传的功能

--- src/main/java/com/ykz/service/impl/FileOptionServiceImpl.java
+++ src/main/java/com/ykz/service/impl/FileOptionServiceImpl.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ResourceUtils;
+import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
@@ -19,27 +20,39 @@ import java.io.IOException;
 @Service
 public class FileOptionServiceImpl implements FileOptionService {
 
-    private static final Logger log = LoggerFactory.getLogger(FileOptionServiceImpl.class);
+    private static final Logger logger = LoggerFactory.getLogger(FileOptionServiceImpl.class);
 
     @Override
     public String uploadFile(MultipartFile file) throws BusinessException {
-
+        String errorMessage = null;
         String origFile = file.getOriginalFilename();
-        if (log.isInfoEnabled()){
-            log.info("开始上传文件:"+origFile);
+        String fileExtension = origFile.substring(origFile.lastIndexOf("."));
+        if (!".xlsx".equals(fileExtension.toLowerCase())) {
+            errorMessage = "文件格式必须是 xlsx";
+            logger.error(errorMessage);
+            throw new BusinessException(EmReturnCode.FAIL, errorMessage);
+
+        }
+        if (logger.isDebugEnabled()){
+            logger.debug("开始上传文件:".concat(origFile));
         }
         String filePath = null;
         try {
             filePath = ResourceUtils.getURL("upload").getPath().concat(File.separator).concat(origFile);
             File saveFile = new File(filePath);
             file.transferTo(saveFile);
-            if (log.isDebugEnabled()){
-                log.debug("文件上传成功："+ filePath);
+            if (logger.isInfoEnabled()){
+                logger.info("文件上传成功：".concat(filePath));
             }
         } catch (IOException e) {
-            String errorMsg = "上传文件：["+ filePath +"] 失败";
-            log.error(errorMsg, e);
-            throw new BusinessException(EmReturnCode.FAIL, errorMsg);
+            errorMessage = "上传文件：["+ filePath +"] 失败";
+            logger.error(errorMessage);
+            throw new BusinessException(EmReturnCode.FAIL, errorMessage);
+        }
+        if (filePath == null){
+            errorMessage = "上传文件：["+ filePath +"] 失败";
+            logger.error(errorMessage);
+            throw new BusinessException(EmReturnCode.FAIL, errorMessage);
         }
         return filePath;
     }
